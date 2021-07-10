4798c20152b03004ef0ffd9123beb317d6101a3f
sindhuja <psindhuja788@gmail.com>
2021-07-10 23:10:10 +0530

    varaiblesfileadded

---
+++ varaibles.tf
@@ -0,0 +1,17 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+variable "amis" {}
+variable "vpc_cidr" {}
+variable "vpc_name" {}
+variable "IGW_name" {}
+variable "key_name" {}
+variable "public_Routing_Table" {}
+variable "private_Routing_Table" {}
+variable "azs" {}
+variable "environment" {}
+variable "instance_type" {}
+
+variable "Public_cidrs" {}
+variable "Private_cidrs" {}
+
